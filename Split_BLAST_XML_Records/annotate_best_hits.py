ac0b104a934bc7d7249acd4d8b2e444449016e4a
Lathian <lathian.j.d.horton@gmail.com>
2016-07-11 09:28:49 -0400

    Ran error trapping lack of one to one mapping to query to report seq.
    It found that every best hit query sequence has a viral report sequence
    that is identical as ME stated. Now I will edit it out and get on
    with the transfer code.

--- Split_BLAST_XML_Records/annotate_best_hits.py
+++ Split_BLAST_XML_Records/annotate_best_hits.py
@@ -15,8 +15,12 @@ from openpyxl import load_workbook
 wb_report = load_workbook(
     'CII-MWP0002.contigs_singletons.blastx.viral_report.xlsx', read_only= False)
 ws_report = wb_report.active
-wb_merge = load_workbook(
+# This workbook will be saved under another name with merge in filename
+wb_best_hit = load_workbook(
     'best_hits_Blastn_viral_queries_against_Lh_TSA.xlsx', read_only= False)
+# To perserve starting input best hit workbook save it to another file
+wb_best_hit.save('merged.xlsx')
+wb_merge = load_workbook('merged.xlsx')
 ws_merge = wb_merge.active
 merge_row = 2
 merge_col = 6
@@ -25,11 +29,27 @@ for i in range(1,31):
     val = ws_report.cell(row = 1 , column = i).value
     merge_cell = ws_merge.cell(row = 2, column = (6 + i -1))
     merge_cell.value = val
-# Scan in all query sequences in report which are in col 20, rows 2 to 4290
-report_queries = []
-for i in range (2, 4291):
-    temp_val = ws_report.cell(row = i, column = 20).value
-    report_queries.append(temp_val)
+# Range of query sequences in best_hits file
+"""found_one_match error trapping code was run ONCE and it showed every best
+    hit query had one and only one match. So this will be edited out after
+    this commit.
+"""
+
+for i in range (3, 4292):
+    # Flag to test that each best hit query sequence find one and only one match
+    found_one_match = False
+    best_hit_query = ws_merge.cell(row = i, column = 2).value
+    for j in range (2, 4291):
+        report_val = ws_report.cell(row = j, column = 20).value
+        if best_hit_query == report_val and found_one_match == False:
+            #print('For Query_' + str(i-2) + 'found a match at Report row ' +
+            #str(j + 2))
+            found_one_match = True
+        elif best_hit_query == report_val and found_one_match == True:
+            print('ERROR! For Query_' + str(i-2) + 
+                   'found ANOTHER match at Report row ' + str(j + 2))
+    if found_one_match == False:
+        print('ERROR! For Query_' + str(i-2) + ' found no match')
 """Range of viral query sequences in report_queries is 0 to 42888. Just add
 2 to get the row in the actual report    
 print(report_queries[0])
@@ -38,6 +58,5 @@ print(report_queries[4288])
 #Now get each best hit query one at a time; look for a match in the 
 # report_queries list. If so call a copy information. Have a boolean
 # flag to make sure there is a match and there is no more then one match.
-
-#wb_merge.save('merged.xlsx')
+wb_merge.save('merged.xlsx')
 print('Program done.')
