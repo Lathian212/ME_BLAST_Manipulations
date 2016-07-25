fec46a9e73df41316e94b01ba4daf5f5cbc6ebb2
Lathian <lathian.j.d.horton@gmail.com>
2016-07-25 08:12:11 -0400

    test_single trying to master SearchIO will need SeqIO I think.

--- Split_BLAST_XML_Records/annotate_best_hits.py
+++ Split_BLAST_XML_Records/annotate_best_hits.py
@@ -30,33 +30,22 @@ for i in range(1,31):
     merge_cell = ws_merge.cell(row = 2, column = (6 + i -1))
     merge_cell.value = val
 # Range of query sequences in best_hits file
-"""found_one_match error trapping code was run ONCE and it showed every best
-    hit query had one and only one match. So this will be edited out after
-    this commit.
-"""
-
 for i in range (3, 4292):
-    # Flag to test that each best hit query sequence find one and only one match
-    found_one_match = False
     best_hit_query = ws_merge.cell(row = i, column = 2).value
+    # Range of query seequence rows in viral report.
     for j in range (2, 4291):
         report_val = ws_report.cell(row = j, column = 20).value
-        if best_hit_query == report_val and found_one_match == False:
-            #print('For Query_' + str(i-2) + 'found a match at Report row ' +
-            #str(j + 2))
-            found_one_match = True
-        elif best_hit_query == report_val and found_one_match == True:
-            print('ERROR! For Query_' + str(i-2) + 
-                   'found ANOTHER match at Report row ' + str(j + 2))
-    if found_one_match == False:
-        print('ERROR! For Query_' + str(i-2) + ' found no match')
-"""Range of viral query sequences in report_queries is 0 to 42888. Just add
-2 to get the row in the actual report    
-print(report_queries[0])
-print(report_queries[4288])
-"""
-#Now get each best hit query one at a time; look for a match in the 
-# report_queries list. If so call a copy information. Have a boolean
-# flag to make sure there is a match and there is no more then one match.
-wb_merge.save('merged.xlsx')
+        if best_hit_query == report_val:
+            #Note columns 22 to 30 are empty after row 3993 so this if is needed
+            if j <= 3993 :
+                for k in range (1, 31):
+                    report_val = ws_report.cell(row = j, column = k).value
+                    query_cell = ws_merge.cell(row = i, column = k +5)
+                    query_cell.value = report_val
+            else:
+                for k in range (1, 22):
+                    report_val = ws_report.cell(row = j, column = k).value
+                    query_cell = ws_merge.cell(row = i, column = k +5)
+                    query_cell.value = report_val
+wb_merge.save('merged')
 print('Program done.')
