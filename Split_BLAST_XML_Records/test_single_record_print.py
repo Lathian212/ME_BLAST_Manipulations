035a1366b9f3be82d502fb277626bfd15abebd6c
Lathian <lathian.j.d.horton@gmail.com>
2016-07-28 13:09:03 -0400

    Working on building worksheet with alignment in test_single code.

--- Split_BLAST_XML_Records/test_single_record_print.py
+++ Split_BLAST_XML_Records/test_single_record_print.py
@@ -1,37 +1,15 @@
-""" OUTDATED SEPARATED INTO TWO DIFFERENT MODULES """
 """ I wrote this script for Python 3.X """
-""" Author: Lathian J.D. Horton (Jonathan S. Kwiat) Date: 7-24-16
+""" Author: Lathian J.D. Horton (Jonathan S. Kwiat) Date: 7-27777777-16
 
-I want to see what the convenience methods 
+I want to see what the convenience methods exist in 
+I intially used SearchIO because it had a writer good for
+slicing records and spitting out individual records. 
  
 """
+from Bio.Blast import NCBIXML
 
 
-
-
-""" Bio.SearchIO is still experimental at this point will throw warning flag,
-    which only means the authors consider it changeable but the virtual
-    evironment will protect against any changes assuming nobody upgrades
-    the bioPython in virtualPython3.5
-"""
-from Bio import SearchIO
-
-
-
-q_g = SearchIO.parse('OUTPUT_Blastn_CII-MWP0002.contigs_singletons.blastx.viral_Queries_against_L_boulardi.xml', 'blast-xml')
-count = 0
-for query_result in q_g:
-    count += 1
-    if count == 60:
-        break
-print(query_result)
-print(query_result.hits[0])
-print(query_result.hits[0].hsps[0])
-
-
-    
-
-
-
-
-
+result_handle = open('OUTPUT_Blastn_CII-MWP0002.contigs_singletons.blastx.viral_Queries_against_L_hetertoma.xml')
+brs = NCBIXML.parse(result_handle)
+br = next(brs)
+print('brs is the generator, br is first record currently')
